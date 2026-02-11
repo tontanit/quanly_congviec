cefeb299f8d19284004fa2e98242da50ab72e228
tontanit <tontanit@gmail.com>
2026-02-11 20:29:18 +0700

    xoa file ko cần thiết

--- quanly_congviec/test_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Nhúng file cấu hình vào
-include 'config/database.php';
-
-if ($conn) {
-    echo "<h1 style='color: green;'>Chúc mừng! Bạn đã kết nối Cơ sở dữ liệu thành công.</h1>";
-    echo "Hệ thống đã sẵn sàng để xây dựng các chức năng tiếp theo.";
-}
